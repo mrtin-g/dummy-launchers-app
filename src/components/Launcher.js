eeba44996ffb7bee2da9025c65462127f02bb246
mgegernieks <mgegernieks@evolutiongaming.com>
2019-08-16 18:03:04 +0300

    moved static contet out of state, cleaned up main.js

--- src/components/Launcher.js
+++ src/components/Launcher.js
@@ -4,13 +4,7 @@ const Launcher = props => {
   return (
     <div style={props.style}>
       <p>{props.name}</p>
-      <img
-        src={props.src}
-        alt="crab"
-        href={props.link}
-        height="130px"
-        width="130px"
-      />
+      <img src={props.src} alt="crab" height="130px" width="130px" />
     </div>
   );
 };
